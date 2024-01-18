ee5e3b487452e4a001d8876967b3dedc7b952f90
Oleh <gend4016@gmail.com>
2024-01-18 19:25:54 +0200

    deploy21

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 import type { Metadata } from "next";
+import Head from "next/head";
 import "./globals.css";
 import { TheFooter } from "@/components/TheFooter"
 import { TheHeader } from "@/components/TheHeader";
@@ -18,6 +19,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+       <Head>
+        <title>xxxcasinoguru</title>
+        <meta name="description" content="casino guru is a very cool site" />
+        {/* Добавляем ссылки на шрифты внутри компонента Head */}
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto:wght@700&display=swap"
+        />
+      </Head>
       <body>
         <TheHeader />
 
