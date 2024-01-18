ec7de2c8268931588e7b16658e1401de12afc332
Oleh <gend4016@gmail.com>
2024-01-18 19:45:44 +0200

    deploy21

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 "use client"
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import "./globals.css";
 import { TheFooter } from "@/components/TheFooter"
 import { TheHeader } from "@/components/TheHeader";
 import './i18n';
 
 
-export const metadata: Metadata = {
-  title: "pickbonus.myawardwallet",
-  description: `The World's First Casino Ecosystem`,
-};
+// export const metadata: Metadata = {
+//   title: "pickbonus.myawardwallet",
+//   description: `The World's First Casino Ecosystem`,
+// };
 
 export default function RootLayout({
   children,
